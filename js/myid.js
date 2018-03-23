77463aec534c879a9adaf8f57d8f8677b6cb74e5
NandoSK8 <nandosk851@gmail.com>
2018-03-23 12:01:35 -0400

    Add files via upload

---
+++ js/myid.js
@@ -0,0 +1,11 @@
+(function($) {
+    $(function() {
+
+        $(".button-collapse").sideNav();
+        $('.dropdown-trigger').dropdown();
+        $('.slider').slider();
+
+
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
